5948efe83473bdd6c6a7bc8e5cba3703cf6e4e7c
leo <leontev.vyacheslav@gmail.com>
2026-03-17 21:45:25 +0300

    feat: map

--- flow-space-ui/src/pages/map-page/map-page.tsx
+++ flow-space-ui/src/pages/map-page/map-page.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useRef } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
 import './map-page.scss';
@@ -6,9 +6,38 @@ import PageHeader from "../../components/page-header/page-header";
 import { AdditionalMenuIcon, MapIcon, RefreshIcon } from "../../constants/app-icons";
 import AppConstants from "../../constants/app-constants";
 import type { MenuItemModel } from "../../models/menu-item-model";
+import { useAppData } from "../../contexts/app-data/app-data";
+import L from "leaflet";
 
 export const MapPage = () => {
     const mapRef = useRef<L.Map>(null);
+    const { getDeviceListAsync } = useAppData();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const devices = await getDeviceListAsync();
+            if (devices && mapRef.current) {
+                const group = L.featureGroup().addTo(mapRef.current);
+
+                devices.forEach(device => {
+                    if (device.objectLocation) {
+                        const { latitude, longitude } = device.objectLocation;
+                        L.marker([latitude, longitude]).addTo(group);
+                    }
+                });
+
+                // Fit map to markers bounds if any markers were added
+                if (group.getLayers().length > 0) {
+                    mapRef.current.fitBounds(group.getBounds(), {
+                        padding: [40, 40],
+                        maxZoom: 15,
+                    });
+                }
+            }
+        };
+        fetchData();
+    }, [getDeviceListAsync]);
+
     const defaultCenter: [number, number] = [51.50853, -0.12574];
 
     const menuItems = useMemo(() => {
