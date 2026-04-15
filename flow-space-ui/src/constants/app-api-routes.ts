e2f3f070880ec890db93d4683ff63facfc7ba06d
leo <leontev.vyacheslav@gmail.com>
2026-04-15 11:22:56 +0300

    fix: web api

--- flow-space-ui/src/constants/app-api-routes.ts
+++ flow-space-ui/src/constants/app-api-routes.ts
@@ -1,5 +1,5 @@
 export default {
-    host: process.env.NODE_ENV !== 'production' ? 'http://localhost:3001' : `http://${window.location.hostname}:3000`,
+    host: process.env.NODE_ENV !== 'production' ? 'http://localhost:3002' : `http://${window.location.hostname}:3000`,
 
     accountSignIn: '/sign-in', //+
     accountSignOut: '/sign-out',
