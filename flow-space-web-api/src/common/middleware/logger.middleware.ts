31a1f566e9bb190559fca5480e45e0ad4765d04a
leo <leontev.vyacheslav@gmail.com>
2026-05-26 06:57:24 +0300

    fix

--- flow-space-web-api/src/common/middleware/logger.middleware.ts
+++ flow-space-web-api/src/common/middleware/logger.middleware.ts
@@ -1,19 +1,20 @@
 // logger.middleware.ts
 import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
+import { STATUS_CODES } from 'http';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  private logger = new Logger('HTTP');
+    private logger = new Logger('HTTP');
 
-  use(req: Request, res: Response, next: NextFunction) {
-    const { method, originalUrl } = req;
+    use(req: Request, res: Response, next: NextFunction) {
+        const { method, originalUrl } = req;
 
-    res.on('finish', () => {
-      // logs AFTER the response is sent, so you get the status code too
-      this.logger.log(`${method} ${originalUrl} → ${res.statusCode}`);
-    });
+        res.on('finish', () => {
+            // logs AFTER the response is sent, so you get the status code too
+            this.logger.log(`${method} ${originalUrl} ${res.statusCode} ${STATUS_CODES[res.statusCode] || ''}`.trim());
+        });
 
-    next();
-  }
+        next();
+    }
 }
