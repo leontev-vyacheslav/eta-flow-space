0dffa9fc5f863c1543601333c64d0ca45ad5f167
leo <leontev.vyacheslav@gmail.com>
2026-06-01 07:29:39 +0300

    fix

--- flow-space-web-api/src/flow/flow.module.ts
+++ flow-space-web-api/src/flow/flow.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { FlowController } from './flow.controller';
 import { FlowDataModel, DeviceDataModel, UserDeviceLinkDataModel } from '../database/models';
 import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
+import { FlowService } from './flow.service';
 
 @Module({
     imports: [SequelizeModule.forFeature([FlowDataModel, DeviceDataModel, UserDeviceLinkDataModel])],
     controllers: [FlowController],
+    providers: [FlowService],
 })
 export class FlowModule {}
