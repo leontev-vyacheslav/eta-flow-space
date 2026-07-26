16143b3ff2d9d8b1e9e96bb64dfd2bc0ea035f18
leo <leontev.vyacheslav@gmail.com>
2026-07-26 09:19:22 +0300

    fix

--- flow-space-web-api/src/database/models/device.data-model.ts
+++ flow-space-web-api/src/database/models/device.data-model.ts
@@ -70,7 +70,7 @@ export class DeviceDataModel extends Model {
     declare lastStateUpdate: Date;
 
     @HasOne(() => MnemoschemaSelectorDataModel, 'deviceId')
-    declare mnemoSchemaSelector?: MnemoschemaSelectorDataModel;
+    declare mnemoschemaSelector?: MnemoschemaSelectorDataModel;
 
     @HasMany(() => DeviceStateDataModel, 'deviceId')
     declare states?: DeviceStateDataModel[];
