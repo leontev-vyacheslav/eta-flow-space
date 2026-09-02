360061c026bb30cb3e2350605557c999269773de
Leo <eta.development.leo@outlook.com>
2026-09-02 08:12:57 +0300

    fix

--- flow-space-reporting/app/modules/router.py
+++ flow-space-reporting/app/modules/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from app.modules.common.controller import router as common_router
-from app.modules.devices.spring2.controller import router as device_spring2_router
+from app.modules.devices.irvis.controller import router as device_irvis_router
 
 router = APIRouter()
-router.include_router(device_spring2_router)
+router.include_router(device_irvis_router)
 router.include_router(common_router)
