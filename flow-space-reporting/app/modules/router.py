08fab3475366cb16ed2e67decedbd749654fa7e9
leo <leontev.vyacheslav@gmail.com>
2026-09-21 12:24:23 +0300

    feat: vzljot026 accountig report

--- flow-space-reporting/app/modules/router.py
+++ flow-space-reporting/app/modules/router.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 from app.modules.common.controller import router as common_router
 from app.modules.devices.irvis.controller import router as device_irvis_router
 from app.modules.devices.mercury230.controller import router as device_mercury230_router
+from app.modules.devices.vzljot026.controller import router as device_vzljot026_router
 
 router = APIRouter()
 router.include_router(device_irvis_router)
+router.include_router(device_vzljot026_router)
 router.include_router(device_mercury230_router)
 router.include_router(common_router)
